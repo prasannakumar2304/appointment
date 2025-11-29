c98a78f4d8cf7399d5fc49a270f03dde2d59ed1a
prasannakumar2304 <128059145+prasannakumar2304@users.noreply.github.com>
2025-11-29 20:04:13 +0530

    Update Patient.js

--- Patient.js
+++ Patient.js
@@ -1,10 +1,20 @@
 const mongoose = require("mongoose");
 
 const patientSchema = new mongoose.Schema({
+  patientId: {
+    type: String,
+    unique: true,
+    sparse: true,
+    required: true
+  },
   name: String,
   email: String,
   phone: String,
-  password: String
+  password: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model("Patient", patientSchema);
